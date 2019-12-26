200b2415bc8928b41348510f2e17fb6b98327c63
kamonotakahashi <kamonotakahashi@gmail.com>
2019-12-26 14:07:32 +0900

    commit

--- RamenProject/src/Lib/SheetSearch.php
+++ RamenProject/src/Lib/SheetSearch.php
@@ -1,19 +1,39 @@
 <?php
 
-require_once dirname(__FILE__) . "/test.php";
+namespace App\Lib;
+
+use App\Lib\SheetAlphabet;
 
 class SheetSearch {
 
     private $sheetId;
+    private $pointer;
+    private $row;
+    private $col;
+    private $alphabetList;
 
-    public function __construct($sheetId)
+    public function search($alphabetList, $move, $beginRowPos, $beginColPos)
     {
-        $this->sheetId = $sheetId;
+        $this->row = $beginRowPos;
+        $this->col = $beginColPos;
+
+        $this->move($move);
+
+        var_dump($this->$pointer);
+
+        //$result = self::$loop();
+
     }
 
-    public function run()
+    private static function move($move) :void
     {
-
+        if($move === 'y') {
+            $this->$pointer = &$this->$row;
+        } else if($move === 'x'){
+            $this->$pointer = &$this->$col;
+        } else {
+            $this->$pointer = null;
+        }
     }
 
     private function loop()
--- RamenProject/src/Services/BusinessHoursService.php
+++ RamenProject/src/Services/BusinessHoursService.php
@@ -3,17 +3,33 @@
 namespace App\Services;
 
 use Noodlehaus\Config;
-use App\Lib\SheetAlphabet;
+
+use App\Lib\SheetSearch;
+
 
 class BusinessHoursService {
 
     private static $config;
 
-    public static function run() :void
+    public static function run()
     {
         self::$config = new Config("config/config.json");
-        $alphabetList = new SheetAlphabet(2);
-        var_dump($alphabetList->getPosAlphabetNumber('AZ'));
+        $alphabetList = new SheetAlphabet(3);
+
+        var_dump(self::getRamenShopIdList($alphabetList));
+
+        //var_dump($alphabetList->getPosAlphabetNumber('AZ'));
+        //var_dump(do_shortcode("[supsystic-tables-cell id=0 row=1 col=C]"));
+    }
+
+    private static function getRamenShopIdList($alphabetList)
+    {
+        $move = self::$config->get("global.ramenShop.searchMoveTarget");
+        $beginRowPos = self::$config->get("global.ramenShop.row");
+        $beginColPos = self::$config->get("global.ramenShop.col");
+
+        return SheetSearch::search($alphabetList, $move, $beginRowPos, $beginColPos);
+
     }
 
 }
