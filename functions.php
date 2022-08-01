59027f3c3f4476eefa271a68f891401375777603
sokorahen-szk <kamonotakahashi@gmail.com>
2022-08-01 20:39:27 +0900

    fix functions.php

--- functions.php
+++ functions.php
@@ -1,18 +1,21 @@
 <?php
+
+// このcommon.phpはサーバに含めない
 include_once dirname(__FILE__) . "/RamenProject/common.php";
 
 /****************************************************************
 ▼　ここから、 営業時間のカテゴリーを変更する
     functions.php 内で使うコード　▼
 ****************************************************************/
-define("VENDOR_PATH", "/RamenProject/vendor/autoload.php");
+function hogehogeeigyoutyuu() {
+    define("VENDOR_PATH", "/RamenProject/vendor/autoload.php");
 
-try {
-    define("VENDOR_DIRECTORY_FULL_PATH_NAME", checkVendorPath() );
-    include_once dirname(__FILE__) . "/RamenProject/main.php";
-} catch (Exception $e) {
-    var_dump($e->getMessage());
-    //Autoloadが読み込みできない場合、例外処理として対応し、継続処理は続ける。システム上影響はでない
+    try {
+        define("VENDOR_DIRECTORY_FULL_PATH_NAME", checkVendorPath() );
+        include_once dirname(__FILE__) . "/RamenProject/main.php";
+    } catch (Exception $e) {
+        error_log(sprintf("categories change error %s", $e->getMessage()), 0);
+    }
 }
 function checkVendorPath() {
     if( file_exists(dirname(dirname(__FILE__)).VENDOR_PATH) ) {
@@ -22,7 +25,8 @@ function checkVendorPath() {
     }
     throw new Exception('Include file does not exists or is not readable.');
 }
+add_action('business_hours_status_change', 'hogehogeeigyoutyuu');
 /****************************************************************
-▲　ここから、 営業時間のカテゴリーを変更する
+▲　ここまで、 営業時間のカテゴリーを変更する
     functions.php 内で使うコード　▲
 ****************************************************************/
